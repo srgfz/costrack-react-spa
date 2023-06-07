683f95892afd969c9314e2692ee2c0251ebafbb4
srgfz <fernandezsergio10@gmail.com>
2023-06-07 21:34:13 +0200

    views

--- src/pages/Bill/Bill.jsx
+++ src/pages/Bill/Bill.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
